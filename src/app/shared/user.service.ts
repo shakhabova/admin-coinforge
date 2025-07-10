50bbed6d85fa0a5ad1c537b47a1a0903cea96962
Alina Shakhabova <alina.shakhabova@yandex.ru>
2025-07-10 15:01:29 +0300

    table fixed

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -5,147 +5,133 @@ import { ConfigService } from './config.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export interface UserInfoDto {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber: string;
-  country: string;
-  city: string;
-  zipCode: string;
-  address: string;
-  status: UserStatus;
-  mfaStatus: MfaStatus;
-  role: string;
-  institutionId: string;
-  createdAt: string;
-  updatedAt: string;
-  customerId: number;
+	id: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	phoneNumber: string;
+	country: string;
+	city: string;
+	zipCode: string;
+	address: string;
+	status: UserStatus;
+	mfaStatus: MfaStatus;
+	role: string;
+	institutionId: string;
+	createdAt: string;
+	updatedAt: string;
+	customerId: number;
 }
 
-export type UserStatus =
-  | 'EMAIL_NOT_CONFIRMED'
-  | 'PENDING'
-  | 'ACTIVE'
-  | 'BLOCKED'
-  | 'REJECTED';
+export interface UsersResponseDto {
+	data: UserInfoDto[];
+	pageNumber: number;
+	pageSize: number;
+	totalElements: number;
+}
+
+export type UserStatus = 'EMAIL_NOT_CONFIRMED' | 'PENDING' | 'ACTIVE' | 'BLOCKED' | 'REJECTED';
 
 export type MfaStatus = 'REJECTED' | 'PENDING' | 'ACTIVATED';
 
 export interface UsersFilterDto {
-  dateFrom?: string;
-  dateTo?: string;
-  customerId?: string;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  country?: string;
-  city?: string;
-  status?: UserStatus;
+	dateFrom?: string;
+	dateTo?: string;
+	customerId?: string;
+	firstName?: string;
+	lastName?: string;
+	email?: string;
+	country?: string;
+	city?: string;
+	status?: UserStatus;
+}
+
+export interface UsersPagination {
+	page: number;
+	pageSize: number;
 }
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class UserService {
-  private httpClient = inject(HttpClient);
-  private configService = inject(ConfigService);
-  private destroyRef = inject(DestroyRef);
+	private httpClient = inject(HttpClient);
+	private configService = inject(ConfigService);
+	private destroyRef = inject(DestroyRef);
 
-  currentUser$ = new BehaviorSubject<UserInfoDto | null>(null);
-  currentUserUpdating$ = new BehaviorSubject<boolean>(false);
-  currentUserId$ = this.currentUser$.pipe(map((info) => info?.id));
+	currentUser$ = new BehaviorSubject<UserInfoDto | null>(null);
+	currentUserUpdating$ = new BehaviorSubject<boolean>(false);
+	currentUserId$ = this.currentUser$.pipe(map((info) => info?.id));
 
-  constructor() {
-    this.updateCurrentUser();
-  }
+	constructor() {
+		this.updateCurrentUser();
+	}
 
-  getAll(filters: Required<UsersFilterDto>): Observable<UserInfoDto[]> {
-    return this.httpClient.get<UserInfoDto[]>(
-      `${this.configService.serverUrl}/v1/users/all`,
-      { params: filters },
-    );
-  }
+	getAll(filters: Required<UsersFilterDto>, pagination: UsersPagination): Observable<UsersResponseDto> {
+		return this.httpClient.get<UsersResponseDto>(`${this.configService.serverUrl}/v1/users/all`, {
+			params: { ...filters, ...pagination },
+		});
+	}
 
-  getInfo(): Observable<UserInfoDto> {
-    return this.httpClient.get<UserInfoDto>(
-      `${this.configService.serverUrl}/v1/users/current`,
-    );
-  }
+	getInfo(): Observable<UserInfoDto> {
+		return this.httpClient.get<UserInfoDto>(`${this.configService.serverUrl}/v1/users/current`);
+	}
 
-  updateCurrentUser() {
-    this.currentUserUpdating$.next(true);
-    this.getInfo()
-      .pipe(
-        takeUntilDestroyed(this.destroyRef),
-        finalize(() => {
-          this.currentUserUpdating$.next(false);
-        }),
-      )
-      .subscribe({
-        next: (info) => this.currentUser$.next(info),
-      });
-  }
+	updateCurrentUser() {
+		this.currentUserUpdating$.next(true);
+		this.getInfo()
+			.pipe(
+				takeUntilDestroyed(this.destroyRef),
+				finalize(() => {
+					this.currentUserUpdating$.next(false);
+				}),
+			)
+			.subscribe({
+				next: (info) => this.currentUser$.next(info),
+			});
+	}
 
-  getUser(email: string): Observable<UserInfoDto> {
-    return this.httpClient.get<UserInfoDto>(
-      `${this.configService.serverUrl}/v1/internal/users`,
-      {
-        params: { email: decodeURIComponent(email) },
-      },
-    );
-  }
+	getUser(email: string): Observable<UserInfoDto> {
+		return this.httpClient.get<UserInfoDto>(`${this.configService.serverUrl}/v1/internal/users`, {
+			params: { email: decodeURIComponent(email) },
+		});
+	}
 
-  clearCurrentUser(): void {
-    this.currentUser$.next(null);
-  }
+	clearCurrentUser(): void {
+		this.currentUser$.next(null);
+	}
 
-  blockUser(userId: number): Observable<void> {
-    return this.httpClient.put<void>(
-      `${this.configService.serverUrl}/v1/users/block`,
-      {
-        currentUserId: this.currentUser$.value?.id,
-        validData: true,
-        userId,
-      },
-    );
-  }
+	blockUser(userId: number): Observable<void> {
+		return this.httpClient.put<void>(`${this.configService.serverUrl}/v1/users/block`, {
+			currentUserId: this.currentUser$.value?.id,
+			validData: true,
+			userId,
+		});
+	}
 
-  unblockUser(userId: number): Observable<void> {
-    return this.httpClient.put<void>(
-      `${this.configService.serverUrl}/v1/users/unblock`,
-      {
-        currentUserId: this.currentUser$.value?.id,
-        validData: true,
-        userId,
-      },
-    );
-  }
+	unblockUser(userId: number): Observable<void> {
+		return this.httpClient.put<void>(`${this.configService.serverUrl}/v1/users/unblock`, {
+			currentUserId: this.currentUser$.value?.id,
+			validData: true,
+			userId,
+		});
+	}
 
-  rejectUser(userId: number): Observable<void> {
-    return this.httpClient.put<void>(
-      `${this.configService.serverUrl}/v1/users/${userId}/reject`,
-      null,
-    );
-  }
+	rejectUser(userId: number): Observable<void> {
+		return this.httpClient.put<void>(`${this.configService.serverUrl}/v1/users/${userId}/reject`, null);
+	}
 
-  activateUser(userId: number): Observable<void> {
-    return this.httpClient.put<void>(
-      `${this.configService.serverUrl}/v1/users/status`,
-      {
-        userIds: [userId],
-        status: 'ACTIVE',
-      },
-    );
-  }
+	activateUser(userId: number): Observable<void> {
+		return this.httpClient.put<void>(`${this.configService.serverUrl}/v1/users/status`, {
+			userIds: [userId],
+			status: 'ACTIVE',
+		});
+	}
 
-  deactivateUser(userId: number): Observable<void> {
-    return this.httpClient.put<void>(
-      `${this.configService.serverUrl}/v1/users/status`,
-      {
-        userIds: [userId],
-        status: 'REJECTED',
-      },
-    );
-  }
+	deactivateUser(userId: number): Observable<void> {
+		return this.httpClient.put<void>(`${this.configService.serverUrl}/v1/users/status`, {
+			userIds: [userId],
+			status: 'REJECTED',
+		});
+	}
 }
